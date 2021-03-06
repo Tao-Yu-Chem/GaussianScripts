1b5a6a849a88ca143b56f742a5a0185f8f73e3b4
Tao-Yu-Chem <64403729+Tao-Yu-Chem@users.noreply.github.com>
2021-03-06 11:41:02 +0800

    Add files via upload

--- Converge_Reader.cpp
+++ Converge_Reader.cpp
@@ -5,7 +5,12 @@
 #include "iostream"
 using namespace std;
 
-#define PRINT_LOWEST_ENERGY if (!LowestEnergyPrinted) LowestEnergyPrinted=1,printf("\033[33mLowest Energy: AbsE = %.6f Hartree, RelaE = %.2f kJ/mol\033[0m\n",E_lowest,2625.5*(E_lowest-Energy_Level))
+#define PRINT_LOWEST_ENERGY printf("\033[33mLowest Energy: AbsE = %.6f Hartree, RelaE = %.2f kcal/mol in step %d\033[0m\n",E_lowest,627.5095*(E_lowest-Energy_Level),i_min_E)
+#define PRINT_LOWEST_MAX_F printf("Minimum MAX_F = %.6f in step %d\t\t",min_MAX_F,i_min_MAX_F)
+#define PRINT_LOWEST_RMS_F printf("Minimum RMS_F = %.6f in step %d\n",min_RMS_F,i_min_RMS_F)
+#define PRINT_LOWEST_MAX_D printf("Minimum MAX_D = %.6f in step %d\t\t",min_MAX_D,i_min_MAX_D)
+#define PRINT_LOWEST_RMS_D printf("Minimum RMS_D = %.6f in step %d\n",min_RMS_D,i_min_RMS_D)
+#define PRINT_LOWEST if (!LowestEnergyPrinted) {LowestEnergyPrinted=1; PRINT_LOWEST_ENERGY; PRINT_LOWEST_MAX_F; PRINT_LOWEST_RMS_F; PRINT_LOWEST_MAX_D; PRINT_LOWEST_RMS_D; }
 
 void ReadLine(FILE *f,int &EOLN, char *Line)
 {
@@ -27,7 +32,7 @@ void DualPrint(double x,double tol,const char *ifYes,const char *ifNo)
 {
 	char Ctrl[50]; const char *p;
 	p=(x<tol)?ifYes:ifNo;
-	sprintf(Ctrl,"\t\033[%sm%%.6f\t%%.6f\033[0m",p);
+	sprintf(Ctrl,"\t\033[%sm%%.6f  %%.6f\033[0m",p);
 	printf(Ctrl,x,tol);
 }	//https://blog.csdn.net/virtual_func/article/details/47707601
 
@@ -38,7 +43,7 @@ void PrintScanList(const vector <double> ScanList, const vector < const char * >
 	{
 		puts("\nScan summary:");
 		for (i=0;i<ScanL;i++)
-			printf("%d\t AbsE = %.6f Hartree, RelaE = %.2f kJ/mol",i,ScanList[i],2625.5*(ScanList[i]-ScanList[0])),
+			printf("%d\t AbsE = %.6f Hartree, RelaE = %.2f kcal/mol",i,ScanList[i],627.5095*(ScanList[i]-ScanList[0])),
 			printf(" \t\033[31m%s\033[0m\n",ScanEWList[i]);
 		puts("");
 	}
@@ -55,18 +60,20 @@ int main(int argc, char *argv[])
 	}
 	char Line[9999];
 	int LowestEnergyPrinted=0;
+	int i_min_E,i_min_MAX_F,i_min_RMS_F,i_min_MAX_D,i_min_RMS_D;
 	int EOLN,i,N=0,flag=1;	// flag=1 indicates the new starting of an opt step or what.
-	char *p;
-	double MAX_F,TOL_MAX_F;
-	double RMS_F,TOL_RMS_F;
-	double MAX_D,TOL_MAX_D;
-	double RMS_D,TOL_RMS_D;
+	int MoreThanSCF=0;
+	char *p,*p2;
+	double MAX_F,TOL_MAX_F,min_MAX_F;
+	double RMS_F,TOL_RMS_F,min_RMS_F;
+	double MAX_D,TOL_MAX_D,min_MAX_D;
+	double RMS_D,TOL_RMS_D,min_RMS_D;
 	double Energy,Energy_Level,E_lowest=1000;
 	vector <double> ScanList;
 	vector < const char * > ScanEWList;
 
 	i=1;
-	printf("\n\t\t\t\t\t\t\t\t\t\t\033[33mAbsE(Hartree)\tRelaE(kJ/mol)\033[0m\nn\tMAX_Force\tThreshold\tRMS_Force\tThreshold\tMAX_Dsplsmnt\tThreshold\tRMS_Dsplsmnt\tThreshold\n");
+	printf("\n  \033[33mAbsE(Hartree)\tRelaE(kcal/mol)\033[0m\nn\tMAX_Force  Threshold\tRMS_Force  Threshold\tMAX_Dsplsmnt  Threshold\tRMS_Dsplsmnt  Threshold\n");
 	while (!feof(f))
 	{
 		ReadLine(f,EOLN,Line);
@@ -74,33 +81,77 @@ int main(int argc, char *argv[])
 		if (p)
 		{
 			ReadLine(f,EOLN,Line);
-			sscanf(strstr(Line,".")-1,"%lf%lf",&MAX_F,&TOL_MAX_F);
+			if (sscanf(strstr(Line,".")-1,"%lf%lf",&MAX_F,&TOL_MAX_F)!=2) continue;
 			ReadLine(f,EOLN,Line);
-			sscanf(strstr(Line,".")-1,"%lf%lf",&RMS_F,&TOL_RMS_F);
+			if (sscanf(strstr(Line,".")-1,"%lf%lf",&RMS_F,&TOL_RMS_F)!=2) continue;
 			ReadLine(f,EOLN,Line);
-			sscanf(strstr(Line,".")-1,"%lf%lf",&MAX_D,&TOL_MAX_D);
+			if (sscanf(strstr(Line,".")-1,"%lf%lf",&MAX_D,&TOL_MAX_D)!=2) continue;
 			ReadLine(f,EOLN,Line);
-			sscanf(strstr(Line,".")-1,"%lf%lf",&RMS_D,&TOL_RMS_D);
+			if (sscanf(strstr(Line,".")-1,"%lf%lf",&RMS_D,&TOL_RMS_D)!=2) continue;
 			printf("%d",i);
 			DualPrint(MAX_F,TOL_MAX_F,"4","0");
 			DualPrint(RMS_F,TOL_RMS_F,"4","0");
 			DualPrint(MAX_D,TOL_MAX_D,"4","0");
 			DualPrint(RMS_D,TOL_RMS_D,"4","0");
 			puts("");
+
+			if (i==1)
+			{
+				min_MAX_F=MAX_F, min_RMS_F=RMS_F, min_MAX_D=MAX_D, min_RMS_D=RMS_D;
+				i_min_MAX_F = i_min_RMS_F = i_min_MAX_D = i_min_RMS_D = i;
+			}
+			if (min_MAX_F>MAX_F) min_MAX_F=MAX_F, i_min_MAX_F=i;
+			if (min_RMS_F>RMS_F) min_RMS_F=RMS_F, i_min_RMS_F=i;
+			if (min_MAX_D>MAX_D) min_MAX_D=MAX_D, i_min_MAX_D=i;
+			if (min_RMS_D>RMS_D) min_RMS_D=RMS_D, i_min_RMS_D=i;
+
 			i++;
 		}
 		p=strstr(Line,"SCF Done:");
-		if (p)
+		if (p && (!MoreThanSCF))
 		{
 			LowestEnergyPrinted=0;
-			sscanf(strstr(Line,"=")+1,"%lf",&Energy);
+			if (sscanf(strstr(Line,"=")+1,"%lf",&Energy)!=1) continue;
 			if (flag)
 			{
 				flag=0;
+				i_min_E = 1;
+				E_lowest=Energy_Level=Energy;	//	This would not interfere a MP2 or double hybrid functional job, since MP2 always gives a negative correction in energy.
+			}
+			printf("  \033[33m%.6f(SCF)\t%.2f\033[0m\n",Energy,627.5095*(Energy-Energy_Level));
+			if (E_lowest>Energy) E_lowest=Energy, i_min_E=i;
+		}
+		p2=strstr(Line,"E2(");
+		if (p2)
+		{
+			p=strstr(p2,"E(");
+			if (!p) continue;
+			int Walker=0;
+			while (p2[Walker])
+			{
+				if (p2[Walker]=='(' || p2[Walker]==')') 
+					p2[Walker]=' ';	//	to enable the following sscanf
+				Walker++;
+			}
+			char Fktional1[300]="",Fktional2[300]="",EnergyStr[300]="",MP2Corr[300]="";
+			if (sscanf(p2,"E2 %s  = %s E %s  = %s",Fktional1,MP2Corr,Fktional2,EnergyStr)!=4) continue;
+			if (strcmp(Fktional1,Fktional2)) continue;
+			int DtoE=0;
+			while(EnergyStr[DtoE])
+			{
+				if (EnergyStr[DtoE]=='d' || EnergyStr[DtoE]=='D') EnergyStr[DtoE]='E';
+				DtoE++;
+			}
+			if (sscanf(EnergyStr,"%lf",&Energy)!=1) continue;
+			if (!MoreThanSCF)
+			{
+				flag=0;
+				i_min_E=1;
+				MoreThanSCF=1;
 				E_lowest=Energy_Level=Energy;
 			}
-			printf("\t\t\t\t\t\t\t\t\t\t\033[33m%.6f\t%.2f\033[0m\n",Energy,2625.5*(Energy-Energy_Level));
-			if (E_lowest>Energy) E_lowest=Energy;
+			printf("  \033[33m%.6f(%s)\t%.2f\033[0m\n",Energy,Fktional1,627.5095*(Energy-Energy_Level));
+			if (E_lowest>Energy) E_lowest=Energy, i_min_E=i;
 		}
 		p=strstr(Line,"Optimization completed");
 		if (p)
@@ -108,11 +159,12 @@ int main(int argc, char *argv[])
 			if (flag) puts("Optimization Completed.\n"); else
 			{
 				printf("\033[32mOptimization Completed with ");
-				PRINT_LOWEST_ENERGY;
+				PRINT_LOWEST;
 				ScanList.push_back(E_lowest);
 				ScanEWList.push_back("");
 			}
 			flag=1;
+			MoreThanSCF=0;
 			i=1;
 		}
 		p=strstr(Line,"Optimization stopped");
@@ -123,11 +175,12 @@ int main(int argc, char *argv[])
 			else
 			{
 				printf("\033[31mOptimization stopped with ");
-				PRINT_LOWEST_ENERGY;
+				PRINT_LOWEST;
 				ScanList.push_back(E_lowest);
 				ScanEWList.push_back("Optimization stopped");
 			}
 			flag=1,i=1;
+			MoreThanSCF=0;
 			ReadLine(f,EOLN,Line);
 			puts(Line);
 			ReadLine(f,EOLN,Line);
@@ -136,7 +189,7 @@ int main(int argc, char *argv[])
 		p=strstr(Line,"Error termination");
 		if (p)
 		{
-			PRINT_LOWEST_ENERGY;
+			PRINT_LOWEST;
 			printf("\033[31m%s\033[0m\n",p);
 			PrintScanList(ScanList,ScanEWList);
 			ScanList.clear();
@@ -147,17 +200,18 @@ int main(int argc, char *argv[])
 		{
 			printf("\033[32m%s\033[0m\n",p);
 			flag=1;
-			PRINT_LOWEST_ENERGY;
+			PRINT_LOWEST;
 			PrintScanList(ScanList,ScanEWList);
 			ScanList.clear();
 			i=1;
+			MoreThanSCF=0;
 		}
 
 	}
 	puts("");
 	if (flag==0)
 	{
-		PRINT_LOWEST_ENERGY;
+		PRINT_LOWEST;
 		ScanList.push_back(E_lowest);
 		ScanEWList.push_back("Optimization still in progress");
 	}
